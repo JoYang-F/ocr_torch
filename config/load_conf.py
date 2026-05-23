6e5d54e6510e6f9886a71770df03254defb43db4
JoYang-F <2652529765@qq.com>
2026-05-23 17:04:53 +0800

    config: 更新训练和预测配置适配新网络
    更新检测/识别训练和推理参数，适配新增的 DBNet 和 ResNet 模块

--- config/load_conf.py
+++ config/load_conf.py
@@ -1,7 +1,6 @@
 import os
 import yaml
 import json
-import codecs
 
 
 class ReadConfig(object):
@@ -11,7 +10,7 @@ class ReadConfig(object):
         self._complement_conf()
 
     def _read_yml_conf(self):
-        with codecs.open(self._yml_path, "r", "utf8") as f:
+        with open(self._yml_path, "r", encoding="utf8") as f:
             conf = yaml.load(f.read(), Loader=yaml.FullLoader)
         return conf
 
@@ -23,13 +22,16 @@ class ReadConfig(object):
             raise Exception("path {} not exists".format(character_json_path))
 
         try:
-            with codecs.open(character_json_path, "r", "utf8") as f:
+            with open(character_json_path, "r", encoding="utf8") as f:
                 char2idx = json.loads(f.read())
         except Exception as e:
             raise e
-        if "<PAD>" not in char2idx.keys():
-            raise Exception("keys <PAD> is not found!")
+
         if "model_det" in self.base_conf.keys():
             self.base_conf["model_det"]["classes_num"] = len(char2idx)
+        if "model" in self.base_conf.keys() and "classes_num" not in self.base_conf["model"].keys():
+            self.base_conf["model"]["classes_num"] = len(char2idx)
+        if "Architecture" in self.base_conf.keys() and "classes_num" not in self.base_conf["Architecture"].keys():
+            self.base_conf["Architecture"]["classes_num"] = len(char2idx)
         if "post_process" in self.base_conf.keys():
             self.base_conf["post_process"]["character_json_path"] = character_json_path
